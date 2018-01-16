e46538ea77f79fe1ac8ae3757c2773174d29efa3
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-15 21:36:33 -0600

    Bonus pattern example pushed for Singleton pattern

---
+++ patternBonus/src/com/premaseem/singleton/ClientForSingletonThreadIssue.java
@@ -0,0 +1,49 @@
+package com.premaseem.singleton;
+
+import java.util.Scanner;
+
+public class ClientForSingletonThreadIssue {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int repeatRunFlag = 1;
+		while (repeatRunFlag == 1) {
+
+			System.out.println("This is the Client Main Singleton Thread issue, there is a fair chance of multiple instiances creation by multiple threads  ");
+
+			Thread thread1 = new Thread() {
+				public void run() {
+					System.out.println("Thread1 Running");
+					SingletonThreadissueInstance.getInstance().doCounting();
+					;
+				}
+			};
+			thread1.start();
+
+			Thread thread2 = new Thread() {
+				public void run() {
+					System.out.println("Thread2 Running");
+					SingletonThreadissueInstance.getInstance().doCounting();
+				}
+			};
+			thread2.start();
+
+			Thread thread3 = new Thread() {
+				public void run() {
+					System.out.println("Thread3 Running");
+					SingletonThreadissueInstance.getInstance().doCounting();
+					;
+				}
+			};
+			thread3.start();
+
+			System.out.println("\n $$$$$$$$$$$$$$$$$$$$  Thanks by Prem Aseem $$$$$$$$$$$$$$$$$$$$$$ \n ");
+			System.out.println("Do you want to Re-run this program - Press 1 for yes and 0 or other digits to EXIT ");
+			try {
+				repeatRunFlag = scan.nextInt();
+			} catch (Exception e) {
+				repeatRunFlag = 0;
+			}
+		}
+	}
+}
