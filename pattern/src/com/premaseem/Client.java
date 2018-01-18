6deb6b92c847bf45b6a3fb99ec07b79e20ed1e45
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-18 11:49:28 -0600

    Base commit with project settings

---
+++ pattern/src/com/premaseem/Client.java
@@ -0,0 +1,13 @@
+package com.premaseem;
+
+/*
+@author: Aseem Jain
+@title: Design Patterns with Java 9
+@link: https://premaseem.wordpress.com/category/computers/design-patterns/
+@copyright: 2018 Packt Publication
+*/
+public class Client {
+    public static void main (String[] args) {
+        System.out.println("Singleton cook example ");
+    }
+}
