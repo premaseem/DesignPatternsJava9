653c0f083c9e2695ca514c5471fa917f6f0c4042
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-18 20:41:18 -0600

    Prototype design pattern implementation on Ganesha statues using mold or breeder class

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -1,5 +1,7 @@
 package com.premaseem;
 
+import java.util.Scanner;
+
 /*
 @author: Aseem Jain
 @title: Design Patterns with Java 9
@@ -7,7 +9,17 @@ package com.premaseem;
 @copyright: 2018 Packt Publication
 */
 public class Client {
-    public static void main (String[] args) {
-        System.out.println("Singleton cook example ");
+    public static void main (String[] args) throws CloneNotSupportedException {
+        System.out.println("### Statue creation by Prototype design pattern ");
+        System.out.println("How many Statue you want to create based on Mold / prototype ?");
+        Scanner scan = new Scanner(System.in);
+        int quantity = scan.nextInt();
+
+        StatueMold statueMold = new GaneshStatue();
+
+        for(int i=0;i<quantity;i++){
+            statueMold.createStatue();
+        }
+
     }
 }
