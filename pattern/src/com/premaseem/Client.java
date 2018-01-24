f55c4b5693cfb3b745d9eac0a9c33a205de84a26
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-23 23:48:12 -0600

    Step 4: client invoking director to build house and return finished product

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -1,13 +1,25 @@
 package com.premaseem;
 
+import com.premaseem.builder.House;
+import com.premaseem.builder.OneBedroomHouseBuilder;
+import com.premaseem.builder.TwoBedroomHouseBuilder;
+import com.premaseem.director.Architect;
+
 /*
 @author: Aseem Jain
 @title: Design Patterns with Java 9
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
-@copyright: 2018 Packt Publication
 */
 public class Client {
     public static void main (String[] args) {
-        System.out.println("Singleton cook example ");
+        System.out.println("Builder Design pattern example ");
+        Architect architect = new Architect();
+
+        // Hiding complexity and building one bed room house
+        architect.setHouseBuilder(new OneBedroomHouseBuilder());
+        architect.buildHouse();
+        House oneBedRoomHouse = architect.getFinishedHouse();
+        System.out.println(oneBedRoomHouse);
+
     }
 }
