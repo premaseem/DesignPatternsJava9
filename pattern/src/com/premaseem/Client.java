8e41ad8d435c048c5e0a79c1bb2fe4959cf56dac
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-31 12:14:05 -0600

    Step 1: Factory pattern - Before code ( poor code )

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -6,8 +6,42 @@ package com.premaseem;
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
 @copyright: 2018 Packt Publication
 */
+
+import com.premaseem.icecreams.ChocolateIceCream;
+import com.premaseem.icecreams.StrawberryIceCream;
+
+import java.util.Scanner;
+
 public class Client {
     public static void main (String[] args) {
-        System.out.println("Singleton cook example ");
+        System.out.println("Ice cream factory example ");
+        System.out.println("Please enter your ice cream choice");
+        System.out.println("Strawberry");
+        System.out.println("Chocolate");
+
+        Scanner scan = new Scanner(System.in);
+
+        // Tight coupling
+        StrawberryIceCream strawberryIceCream =null;
+        ChocolateIceCream chocolateIceCream = null;
+
+        // Sphegati code with if else ladder
+        String iceCreamChoice =   scan.next();
+        if (iceCreamChoice.equalsIgnoreCase("Strawberry")){
+            strawberryIceCream = new StrawberryIceCream();
+        }else if (iceCreamChoice.equalsIgnoreCase("Chocolate")){
+            chocolateIceCream = new ChocolateIceCream();
+        }
+
+        // Ice cream of your choice is :
+        System.out.print("Ice cream of your choice is ");
+        if (strawberryIceCream != null){
+            System.out.println(strawberryIceCream);
+        }
+
+        if (chocolateIceCream != null){
+            System.out.println(chocolateIceCream);
+        }
+
     }
 }
