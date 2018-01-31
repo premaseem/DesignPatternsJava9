f5ca54cd402f0c352ec612787a617091b43ac6a9
Aseem Jain <premaseem.webmaster@gmail.com>
2018-01-31 14:22:25 -0600

    Step 2: Abstract Ice cream interface (Generalize it)

--- pattern/src/com/premaseem/icecreams/ChocolateIceCream.java
+++ pattern/src/com/premaseem/icecreams/ChocolateIceCream.java
@@ -5,13 +5,33 @@ package com.premaseem.icecreams;
 @title: Design Patterns with Java 9
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
 */
-public class ChocolateIceCream {
-    String brand = "un branded";
-    Integer cost = 3;
-    Integer calories = 200;
+public class ChocolateIceCream implements IceCream {
+
+    public ChocolateIceCream (Integer cost, Integer calories){
+        this.cost =cost;
+        this.calories =calories;
+    }
+
+    String brand = "";
+    Integer cost = 0;
+    Integer calories = 0;
+
+    public String toString () {
+        return this.getClass().getSimpleName() + " with Calories: " + getCalories() + " and cost: $" + getCost();
+    }
 
     @Override
-    public String toString () {
-        return this.getClass().getName() + " with " + calories + " and cost: $" + cost;
+    public Integer getCalories () {
+        return calories;
+    }
+
+    @Override
+    public Integer getCost () {
+        return cost;
+    }
+
+    @Override
+    public String getIceCreamName () {
+        return this.getClass().getSimpleName();
     }
 }
