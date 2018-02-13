c07e7717cf75b4c7a6de28445e4136a4a45c5fd9
Aseem Jain <premaseem.webmaster@gmail.com>
2018-02-12 23:23:48 -0600

    Before code for Adapter design pattern - iphone ear plug example

---
+++ pattern/src/com/premaseem/EarPlug.java
@@ -0,0 +1,15 @@
+package com.premaseem;
+
+/*
+@author: Aseem Jain
+@title: Design Patterns with Java 9
+@link: https://premaseem.wordpress.com/category/computers/design-patterns/
+*/
+public class EarPlug {
+
+    public void takeSoundInput(String inputSignal){
+        System.out.println("playing input signal");
+        System.out.println(inputSignal);
+    }
+
+}
---
+++ pattern/src/com/premaseem/phones/GooglePhone.java
@@ -0,0 +1,14 @@
+package com.premaseem.phones;
+
+/*
+@author: Aseem Jain
+@title: Design Patterns with Java 9
+@link: https://premaseem.wordpress.com/category/computers/design-patterns/
+*/
+public class GooglePhone {
+
+    public Integer getSoundOutput(){
+
+        return 1001010101;
+    }
+}
