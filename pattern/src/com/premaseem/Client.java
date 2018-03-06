2116da6d131b4db909ed20207318fcded9db168a
Aseem Jain <premaseem.webmaster@gmail.com>
2018-03-05 18:54:33 -0600

    Command design pattern - BEFORE CODE - Light switch project Lot of coupling and commands cannot be added dynamically

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -1,13 +1,33 @@
 package com.premaseem;
 
+import java.util.Scanner;
+
 /*
 @author: Aseem Jain
 @title: Design Patterns with Java 9
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
-@copyright: 2018 Packt Publication
 */
 public class Client {
     public static void main (String[] args) {
-        System.out.println("Singleton cook example ");
+        Scanner scanInput = new Scanner(System.in);
+        System.out.println("Command Pattern Light switch project ");
+        System.out.println("Master - what command you want to execute ");
+        String command = scanInput.next();
+
+        // Assume you will invoke a method to execute turn on method
+        if (command.equalsIgnoreCase("LightOn")){
+            System.out.println("Light turned on");
+        }
+        else if (command.equalsIgnoreCase("LightOff")){
+            System.out.println("Light turned off");
+        }
+        else {
+            System.out.println(command+ " Command not found");
+        }
+
+        // Lessons Learnt - BEFORE CODE
+        // Lots of if else ladder
+        // Commands cannot be added Dynamically
+        // Code is tightly coupled, no separation of responsibility
     }
 }
