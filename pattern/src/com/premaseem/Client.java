d2afa805cc0f3b146931c42649b304d3122e98db
Aseem Jain <premaseem.webmaster@gmail.com>
2018-03-05 19:13:37 -0600

    Command design pattern - AFTER CODE - Light switch project Loose coupling and commands can be added dynamically with almost zero code changes in client file

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -10,10 +10,31 @@ import java.util.Scanner;
 public class Client {
     public static void main (String[] args) {
         Scanner scanInput = new Scanner(System.in);
-        System.out.println("Command Pattern Light switch project ");
-        System.out.println("Master - what command you want to execute ");
-        String command = scanInput.next();
+        System.out.println("Light switch Project using COMMAND DESIGN PATTERN \n ");
 
+       /** After implementing Command design pattern **/
+
+       // All available Commands will be created and init by command factory
+       CommandFactory cf = CommandFactory.init();
+
+       // Will list all commands which can be executed
+       cf.listCommands();
+
+       // Commands can be added and listed dynamically with ZERO code changes in client side
+       System.out.println("Master - what command you want to execute ");
+       String command = scanInput.next();
+
+       // Actual execution of command will happen
+       cf.executeCommand(command);
+
+        /** Lessons Learnt - AFTER CODE
+         # New commands can be added dynamically with ease
+         # Client is loosely coupled and have no knowledge of encapsulated commands
+         # Zero code changed needed at client when new commands are added
+         */
+
+
+       /** BEFORE CODE
         // Assume you will invoke a method to execute turn on method
         if (command.equalsIgnoreCase("LightOn")){
             System.out.println("Light turned on");
@@ -29,5 +50,6 @@ public class Client {
         // Lots of if else ladder
         // Commands cannot be added Dynamically
         // Code is tightly coupled, no separation of responsibility
+        */
     }
 }
