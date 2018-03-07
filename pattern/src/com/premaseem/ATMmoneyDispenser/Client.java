50fa55c4f21f248d6af9470966eacff3a6e8fccf
Aseem Jain <premaseem.webmaster@gmail.com>
2018-03-07 10:55:55 -0600

    Chain of Responsibility design pattern - Money Dispenser Project

--- pattern/src/com/premaseem/ATMmoneyDispenser/Client.java
+++ pattern/src/com/premaseem/ATMmoneyDispenser/Client.java
@@ -15,29 +15,25 @@ public class Client {
 		System.out.println("Welcome to ATM Money dispenser program designed using Chain of Responsibility");
 		Scanner scan = new Scanner(System.in);
 		
-		MoneyChainHandler thousandollarHandler_1000 = new ThousandollarHandler_1000(1000);
-		MoneyChainHandler FiveHundredollarHandler_500 = new FiveHundredollarHandler_500(500);
-		MoneyChainHandler HundrendollarHandler_100 = new HundrenDollarHandler_100(100);
-		MoneyChainHandler FiftydollarHandler_50 = new FiftyDollarHandler_50(50);
-		MoneyChainHandler TendollarHandler_10 = new TenDollarHandler_10(10);
-		MoneyChainHandler FivedollarHandler_5 = new FiveDollarHandler_5(5);
-		MoneyChainHandler TwodollarHandler_2 = new TwoDollarHandler_2(2);
-		MoneyChainHandler OnedollarHandler_1 = new OneDollarHandler_1(1);
+		MoneyChainHandler hundredDollarHandler_100 = new HundrenDollarHandler_100(100);
+		MoneyChainHandler fiftyDollarHandler_50 = new FiftyDollarHandler_50(50);
+		MoneyChainHandler tenDollarHandler_10 = new TenDollarHandler_10(10);
+		MoneyChainHandler fiveDollarHandler_5 = new FiveDollarHandler_5(5);
+		MoneyChainHandler twoDollarHandler_2 = new TwoDollarHandler_2(2);
+		MoneyChainHandler oneDollarHandler_1 = new OneDollarHandler_1(1);
 		
 		// Setting up the change
-		thousandollarHandler_1000.setNextHandler(FiveHundredollarHandler_500);
-		FiveHundredollarHandler_500.setNextHandler(HundrendollarHandler_100);
-		HundrendollarHandler_100.setNextHandler(FiftydollarHandler_50);
-		FiftydollarHandler_50.setNextHandler(TendollarHandler_10);
-		TendollarHandler_10.setNextHandler(FivedollarHandler_5);
-		FivedollarHandler_5.setNextHandler(TwodollarHandler_2);
-		TwodollarHandler_2.setNextHandler(OnedollarHandler_1);
+		hundredDollarHandler_100.setNextHandler(fiftyDollarHandler_50);
+		fiftyDollarHandler_50.setNextHandler(tenDollarHandler_10);
+		tenDollarHandler_10.setNextHandler(fiveDollarHandler_5);
+		fiveDollarHandler_5.setNextHandler(twoDollarHandler_2);
+		twoDollarHandler_2.setNextHandler(oneDollarHandler_1);
 
 		String repeatRunFlag = "y";
 		while (!repeatRunFlag.equalsIgnoreCase("no")) {
 			System.out.println("Please enter amount you want to withdraw eg 3450 ");
 			int choice = scan.nextInt();
-			thousandollarHandler_1000.handler(choice);
+            hundredDollarHandler_100.handler(choice);
 
 			System.out.println("=============================");
 
