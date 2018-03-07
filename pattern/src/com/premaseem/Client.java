e5b6dbf8effb496b8ee54e3f503559c6ee5acf45
Aseem Jain <premaseem.webmaster@gmail.com>
2018-03-07 13:22:48 -0600

    Interpreter Design Pattern - Artificial Intelligence Chat Bot Checked in client file

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -1,13 +1,28 @@
 package com.premaseem;
 
+import java.util.Scanner;
+
 /*
 @author: Aseem Jain
 @title: Design Patterns with Java 9
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
-@copyright: 2018 Packt Publication
 */
 public class Client {
     public static void main (String[] args) {
-        System.out.println("Singleton cook example ");
+        System.out.println("Alexa welcomes you to Artificial intelligence app designed using Interpreter Design pattern :-) ");
+        Scanner scanner = new Scanner(System.in);
+
+        // User input block
+        String repeatRunFlag = "yes";
+        while (!repeatRunFlag.equalsIgnoreCase("no")) {
+            System.out.println("\n Please ask your question related to voting ...");
+            String question = scanner.nextLine();
+            Boolean result =true;
+            String voteEligible = result ? "Yes ! can vote :-) ":"No ! cannot vote :-)";
+
+            System.out.println("Alexa answered: " + voteEligible);
+            System.out.println("Do you have another questions ? yes / no ");
+            repeatRunFlag = scanner.next();
+        }
     }
 }
