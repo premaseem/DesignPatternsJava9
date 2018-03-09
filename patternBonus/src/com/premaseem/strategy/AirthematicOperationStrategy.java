80611c4ceee6107172eb29efd60b5ea30ce8f03d
Aseem Jain <premaseem.webmaster@gmail.com>
2018-03-09 13:13:15 -0600

    Strategy Design Pattern - Calculator application

---
+++ patternBonus/src/com/premaseem/strategy/AirthematicOperationStrategy.java
@@ -0,0 +1,13 @@
+package com.premaseem.strategy;
+
+/*
+@author: Aseem Jain
+@title: Design Patterns with Java 9
+@link: https://premaseem.wordpress.com/category/computers/design-patterns/
+*/
+
+public interface AirthematicOperationStrategy {
+
+	Integer performOperation (Integer num1, Integer num2);
+	
+}
